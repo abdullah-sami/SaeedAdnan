1faa3b7247c5b105c387f566b3ff463cb10a2c23
abdullah-sami <sailor.lenovo@outlook.com>
2024-09-27 18:22:11 +0600

    update 2

--- resources/js/content.js
+++ resources/js/content.js
@@ -1,20 +1,16 @@
 const root_dir = "resources/img/artworks/";
 
 let gallery_imgs = [
-
+  
   { src: `${root_dir}android_things.webp`, title: "Tech setup", desc: "A sleek collection of modern gadgets (not for a apple user) including a tablet, smartwatches, camera, earbuds, and a gaming phone, neatly arranged on a reflective surface.", tag: "Models Low-poly" },
   
-  { src: `${root_dir}apple_things.webp`, title: "Apple Setup", desc: "A stylish assortment of Apple devices including an iPhone, iPad with apple pencil, Apple watch, AirPods, Apple air tag,and a classic camera, all meticulously organized on a smooth reflective surface.", tag: "Assets High-poly" },
+  { src: `${root_dir}shield.webp`, title: "Guardian Shields", desc: "A pair of sturdy wooden shields, each with a different design. The left shield features two metal braces for added protection, while the right shield has a central metal hub with a glowing orb in the center. The shields are said to be enchanted with protective magic, making them nearly impenetrable to enemy attacks.", tag: "Models Low-poly Weapon" },
   
   { src: `${root_dir}HoverScout.webp`, title: "Explorer Robo", desc: "A cube-shaped robot with mechanical arms and a wheel for movement, traversing a mysterious and foggy landscape in a futuristic world.", tag: "Game Assets 3D" },
   
   { src: `${root_dir}futuristic_chess_console.webp`, title: "Sci-fi Chess Board", desc: "A high-tech, glowing chessboard with advanced mechanisms and bold geometric lines for futuristic gameplay.", tag: "Game Weapon Sci-fi" },
   
-  { src: `${root_dir}white_car_studio.webp`, title: "Mercedes-Benz 300 SL Gullwing [Isometric]", desc: "A sleek vintage car set in a modern photography studio, capturing its timeless design under studio lights.", tag: "Models High-poly Isometric" },
-  
   { src: `${root_dir}Yellow_car_studio.webp`, title: "Volvo P1800 [Isometric]", desc: "A Volvo P1800 in a futuristic yellow studio, highlighting the timeless charm of vintage automotive design under modern lighting.", tag: "Assets High-poly Isometric" },
-
-  { src: `${root_dir}red_car_studio.webp`, title: " Ferrari 488 GTE [Isometric]", desc: "A striking red racing car is showcased in a vibrant room with circular windows, surrounded by modern decoration and creative lighting, highlighting its dynamic design.", tag: "Game Assets 3D Isometric" },
   
   { src: `${root_dir}basketball_court.webp`, title: "BasketBall Court [Isometric]", desc: " A vibrant basketball court, adorned with dynamic graffiti and essential equipment, captures the essence of urban athleticism and community spirit.", tag: "Weapon Sci-fi Isometric" },
   
@@ -22,19 +18,21 @@ let gallery_imgs = [
   
   { src: `${root_dir}home_office.webp`, title: "Home Office [Isometric]", desc: "A functional and inviting home office, featuring a minimalist desk, comfortable seating, and a well-stocked bookshelf, provides a productive and inspiring workspace.", tag: "Game Assets 3D Isometric" },
   
-  { src: `${root_dir}living_room.webp`, title: "Living Room [Isometric]", desc: "A cozy living room, adorned with traditional furnishings, intricate calligraphy, and warm lighting, offers a welcoming and inviting atmosphere.", tag: "Isometric Models" },
-  
   { src: `${root_dir}gaming_room.webp`, title: "Gaming Room [Isometric]", desc: "A futuristic gaming den, featuring a high-tech setup, immersive decor, and comfortable seating, provides the ultimate escape for gaming enthusiasts.", tag: "Models High-poly Isometric" },
   
   { src: `${root_dir}police_car_studio.webp`, title: "Police SUV [Isometric]", desc: "A modern car studio, showcasing a sleek and futuristic electric car model, features state-of-the-art equipment, professional lighting, and a backdrop designed to highlight the vehicle's innovative design.", tag: "Assets High-poly Isometric" },
+  
+  { src: `${root_dir}living_room.webp`, title: "Living Room [Isometric]", desc: "A cozy living room, adorned with traditional furnishings, intricate calligraphy, and warm lighting, offers a welcoming and inviting atmosphere.", tag: "Isometric Models" },
   
   { src: `${root_dir}concert_hall.webp`, title: "Concert Stage [Isometric]", desc: "A futuristic and immersive event space, featuring a large Ethereum logo projection, a dynamic stage setup, and comfortable seating, is ready to host a vibrant and exciting launch party for the Ethereum community.", tag: "Game Assets 3D Isometric" },
   
   { src: `${root_dir}chess_championshin_room.webp`, title: "Chess Championship Room [Isometric]", desc: "A professional chess championship studio, featuring a large chessboard, a live broadcast setup, and spectator seating, provides an immersive experience for viewers and participants alike.", tag: "Isometric High-poly Sci-fi" },
   
+  { src: `${root_dir}red_car_studio.webp`, title: " Ferrari 488 GTE [Isometric]", desc: "A striking red racing car is showcased in a vibrant room with circular windows, surrounded by modern decoration and creative lighting, highlighting its dynamic design.", tag: "Game Assets 3D Isometric" },
+  
   { src: `${root_dir}Crusher.webp`, title: "Morningstar", desc: "A mace-like weapon with a cylindrical head covered in sharp, pyramid-shaped spikes. The head is attached to a wooden handle with metal rings at the top and bottom. The background is a textured, gray wall.", tag: "Untextured Weapon Sci-fi" },
   
-  { src: `${root_dir}Axe.webp`, title: "Double-Bladed Battle Axe", desc: "A fearsome double-bladed battle axe, with a central spike and a red-wrapped wooden handle. Ideal for close-quarters combat, this weapon's design is both intimidating and visually striking.", tag: "Weapon Sci-fi" },
+  { src: `${root_dir}white_car_studio.webp`, title: "Mercedes-Benz 300 SL Gullwing [Isometric]", desc: "A sleek vintage car set in a modern photography studio, capturing its timeless design under studio lights.", tag: "Models High-poly Isometric" },
   
   { src: `${root_dir}Spear.webp`, title: "Glaive of the Crimson Dawn", desc: "A fearsome polearm with a long, curved blade and a wooden shaft wrapped in red and gold cord. The blade's edge is serrated, and the tip is sharp and pointed. The weapon is said to have been forged in the heart of a volcano and imbued with the power of fire.", tag: "Models Low-poly Weapon" },
   
@@ -42,9 +40,11 @@ let gallery_imgs = [
   
   { src: `${root_dir}Sword_yellow.webp`, title: "Sunblade", desc: " A majestic sword with a long, straight blade that shimmers with a golden light. The hilt is adorned with intricate carvings and a glowing gem at the pommel. The blade is said to be forged from the heart of a star, and its light can blind enemies and heal the wounded.", tag: "Game Assets 3D" },
   
-  { src: `${root_dir}Sword_silver.webp`, title: "Shadowblade", desc: "A sleek and deadly blade with a long, straight edge and a black, textured grip. The blade is designed to be lightweight and easily concealed, making it ideal for stealthy combat. The blade is said to be capable of cutting through almost anything, and its sharp edge can inflict devastating wounds.", tag: "Assets Weapon Sci-fi" },
+  { src: `${root_dir}Axe.webp`, title: "Double-Bladed Battle Axe", desc: "A fearsome double-bladed battle axe, with a central spike and a red-wrapped wooden handle. Ideal for close-quarters combat, this weapon's design is both intimidating and visually striking.", tag: "Weapon Sci-fi" },
   
-  { src: `${root_dir}shield.webp`, title: "Guardian Shields", desc: "A pair of sturdy wooden shields, each with a different design. The left shield features two metal braces for added protection, while the right shield has a central metal hub with a glowing orb in the center. The shields are said to be enchanted with protective magic, making them nearly impenetrable to enemy attacks.", tag: "Models Low-poly Weapon" },
+  { src: `${root_dir}apple_things.webp`, title: "Apple Setup", desc: "A stylish assortment of Apple devices including an iPhone, iPad with apple pencil, Apple watch, AirPods, Apple air tag,and a classic camera, all meticulously organized on a smooth reflective surface.", tag: "Assets High-poly" },
+  
+  { src: `${root_dir}Sword_silver.webp`, title: "Shadowblade", desc: "A sleek and deadly blade with a long, straight edge and a black, textured grip. The blade is designed to be lightweight and easily concealed, making it ideal for stealthy combat. The blade is said to be capable of cutting through almost anything, and its sharp edge can inflict devastating wounds.", tag: "Assets Weapon Sci-fi" },
   
   { src: `${root_dir}Hammer.webp`, title: "Thunderstrike", desc: "A mighty warhammer with a heavy metal head and a long, red-wrapped handle. The head is adorned with gold accents and a glowing red gem in the center. The hammer is said to be capable of shattering stone and metal with a single blow, and its thunderous impact can stun enemies and shake the earth.", tag: "Assets High-poly Weapon" },
   
@@ -64,7 +64,7 @@ let gallery_imgs = [
   
 { src: `${root_dir}car.webp`, title: "Neon Nights", desc: "A sleek, modern sports car bathed in the glow of neon lights. The car's dark exterior contrasts beautifully with the vibrant blue and red accents, creating a striking and futuristic aesthetic. The car appears to be parked in a dimly lit garage or underground parking lot.", tag: "Models 3D" },
   
-{ src: `${root_dir}car_2.webp`, title: "1972 Datsun 240K GT", desc: "A classic 1972 Datsun 240K GT, showcased in a minimalist, dimly lit studio. The car's sleek lines and timeless design are highlighted by the dark, reflective surface it's parked on. The "Hakosuka" nickname, derived from the Japanese word "hako" meaning "box," is a nod to the car's distinctive boxy shape.", tag: "High-poly Sci-fi" },
+{ src: `${root_dir}car_2.webp`, title: "1972 Datsun 240K GT", desc: "A classic 1972 Datsun 240K GT, showcased in a minimalist, dimly lit studio. The car's sleek lines and timeless design are highlighted by the dark, reflective surface it's parked on. The 'Hakosuka' nickname, derived from the Japanese word 'hako' meaning 'box,' is a nod to the car's distinctive boxy shape.", tag: "High-poly Sci-fi" },
   
 { src: `${root_dir}Portion.webp`, title: "Elixir of Eternal Youth", desc: "A small, round glass vial filled with a vibrant purple liquid that bubbles and shimmers slightly. Sealed with a cork, the vial is said to contain a powerful elixir capable of granting eternal youth to anyone who drinks it. The liquid is rumored to have been created by ancient alchemists using rare and magical ingredients.", tag: "3D Models" },
   
@@ -76,7 +76,7 @@ let gallery_imgs = [
 
 { src: `${root_dir}Camera.webp`, title: "Lens of Possibilities", desc: "A Canon EOS 5D Mark IV camera surrounded by a variety of interchangeable lenses, all suspended in mid-air against a dark background. The image captures the versatility and creativity of photography, showcasing the different perspectives and focal lengths that can be achieved with a single camera body.", tag: "Models 3D Sci-fi" },
   
-{ src: `${root_dir}Headphone.webp`, title: "Tech and Treasure", desc: "A laptop with a vibrant, colorful display sits open on a desk, illuminated by the surrounding neon lights. Next to it lies a worn, leather-bound book titled "The Treasure," hinting at a hidden adventure. A pair of stylish headphones with a rainbow-colored LED strip rests nearby, ready to immerse the user in a world of music or gaming.", tag: "3D Models Sci-fi" },
+{ src: `${root_dir}Headphone.webp`, title: "Tech and Treasure", desc: "A laptop with a vibrant, colorful display sits open on a desk, illuminated by the surrounding neon lights. Next to it lies a worn, leather-bound book titled 'The Treasure,' hinting at a hidden adventure. A pair of stylish headphones with a rainbow-colored LED strip rests nearby, ready to immerse the user in a world of music or gaming.", tag: "3D Models Sci-fi" },
 
 { src: `${root_dir}Playstation5.webp`, title: "Heart of Gaming", desc: "A sleek, white PlayStation 5 console stands tall against a dark background, illuminated by a blue light. A matching DualSense controller rests nearby, showcasing the console's futuristic design and cutting-edge technology. The image evokes a sense of excitement and anticipation for the next generation of gaming experiences.", tag: "Models Assets 3D" },
   
